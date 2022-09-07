5e9a8b2de82c0091ae1eb4ad4794979fa1b890a9
dvnik147 <dvnik147@hotmail.com>
2022-09-07 11:50:19 +0800

    20220907 practice

--- Book02/HelloWorld/src/main/kotlin/Main.kt
+++ Book02/HelloWorld/src/main/kotlin/Main.kt
@@ -4,4 +4,16 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    var stringArray = arrayOf("a", "b", "c")
+    for (i in (1..10).reversed()) {
+        println("$i")
+    }
+    for (i in 10.downTo(1)) {
+        println("$i")
+    }
+
+    var testNullable:Int?
+    if (val a = testNullable!) {
+        println("$a")
+    }
 }
