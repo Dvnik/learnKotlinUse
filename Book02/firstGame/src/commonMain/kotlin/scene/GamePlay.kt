7bc1e6f4c4008af7dc9180bd8ddd929e06ea007a
dvnik147 <dvnik147@hotmail.com>
2022-09-16 11:33:27 +0800

    finish animation sprite lesson

--- Book02/firstGame/src/commonMain/kotlin/scene/GamePlay.kt
+++ Book02/firstGame/src/commonMain/kotlin/scene/GamePlay.kt
@@ -1,8 +1,10 @@
 package scene
 
 import ConfigModule
+import com.soywiz.klock.*
 import com.soywiz.korge.input.*
 import com.soywiz.korge.scene.*
+import com.soywiz.korge.tween.*
 import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.*
 import com.soywiz.korge.view.onClick
@@ -18,18 +20,28 @@ class GamePlay() : Scene() {
     val buttonPos = Point(128, 128 + 32)
 
     override suspend fun SContainer.sceneInit() {
-        //加入圖片
-        image(resourcesVfs["sandCar.jpeg"].readBitmap()) {
-            scaledWidth = ConfigModule.size.width.toDouble()
-            scaledHeight = ConfigModule.size.height.toDouble()
-            anchor(0.5, 0.5)//改變圖片定錨點的位置，值的範圍:0.0~1.0。0.5就是中心點的位置。
-            //以定錨點為中心在座標點(0,0)呈現圖片，其結果會變成在左上角(默認座標點起始位置)呈現圖片中心點，部分圖片超出視窗
-            //因此還要將圖片移動到畫面中心，才會再次填滿整個視窗
-            position(scaledWidth/2, scaledHeight/2)
-            //Angle的init現在是隱藏值，且rotation這個值的屬性是final
-            //改成這樣才會旋轉
-            rotation(Angle.fromRadians(45.0))
-        }
+        //加入Sprite Animation圖片
+        val spirteMap = resourcesVfs["green_alien_walk.png"].readBitmap()
+        val alienWalkAnimation = SpriteAnimation(
+            spriteMap = spirteMap,
+            spriteWidth = 72,
+            spriteHeight = 97,
+            marginTop = 0,
+            marginLeft = 0,
+            columns = 11,
+            rows = 1,
+            offsetBetweenColumns = 0,
+            offsetBetweenRows = 0
+        )
+        val alien = sprite( alienWalkAnimation )
+        alien.spriteDisplayTime = 0.1.seconds
+        alien.playAnimationLooped()
+
+        //行走動畫
+//        while (true) {
+//            alien.tween(alien::x[0.0,512.0], time = 5.seconds)
+//            delay(1.seconds)
+//        }
 
         //顯示目前的畫面
         text("I'm in ${GamePlay::class.simpleName}") {
@@ -42,5 +54,7 @@ class GamePlay() : Scene() {
                 launchImmediately { sceneContainer.changeTo<GameOver>() }
             }
         }
+
+
     }
 }
--- Book02/firstGame/src/commonMain/kotlin/scene/Menu.kt
+++ Book02/firstGame/src/commonMain/kotlin/scene/Menu.kt
@@ -5,7 +5,9 @@ import com.soywiz.korge.scene.*
 import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.*
 import com.soywiz.korge.view.onClick
+import com.soywiz.korim.format.*
 import com.soywiz.korio.async.*
+import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.*
 
 class Menu() : Scene() {
@@ -15,6 +17,18 @@ class Menu() : Scene() {
     val buttonPos = Point(128, 128 + 32)
 
     override suspend fun SContainer.sceneInit() {
+        //加入圖片
+        image(resourcesVfs["sandCar.jpeg"].readBitmap()) {
+            scaledWidth = ConfigModule.size.width.toDouble()
+            scaledHeight = ConfigModule.size.height.toDouble()
+            anchor(0.5, 0.5)//改變圖片定錨點的位置，值的範圍:0.0~1.0。0.5就是中心點的位置。
+            //以定錨點為中心在座標點(0,0)呈現圖片，其結果會變成在左上角(默認座標點起始位置)呈現圖片中心點，部分圖片超出視窗
+            //因此還要將圖片移動到畫面中心，才會再次填滿整個視窗
+            position(scaledWidth/2, scaledHeight/2)
+            //Angle的init現在是隱藏值，且rotation這個值的屬性是final
+            //改成這樣才會旋轉
+            rotation(Angle.fromRadians(45.0))
+        }
         //顯示目前的畫面
         text("I'm in ${Menu::class.simpleName}") {
             position(textPos)
